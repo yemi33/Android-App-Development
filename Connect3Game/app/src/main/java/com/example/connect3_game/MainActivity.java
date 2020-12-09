b18f73268b5295b0f85c3aa90ec313c913d7625e
Yemi Shin <shiny@carleton.edu>
2020-12-09 04:08:21 -0600

    adding connect3

---
+++ Connect3Game/app/src/main/java/com/example/connect3_game/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.connect3_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+
+        counter.set
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
